151cce45e5df5371c4b015ebdcfcca506166af8b
RedLime <redlimerl@gmail.com>
2021-08-13 18:26:08 +0900

    fixed bat

--- run.bat
+++ run.bat
@@ -3,7 +3,7 @@ cls
 echo This script will keep your bot running even after crashing!
 title DemonObserver Watchdog
 :StartServer
-start /wait node app.js
+start /wait npm start
 echo (%time%) Bot shutdowned or crashed... restarting!
 timeout 3
 goto StartServer
